363281b2c48cfcd2fc1e8d2ea0e401263a8c9b84
DappellaNagavinay2003 <127031845+DappellaNagavinay2003@users.noreply.github.com>
2023-06-28 22:17:52 +0530

    Create Basic_datatypes.java
    This are basic data types declaration and there intialization that every body have to learn

---
+++ Basic_datatypes.java
@@ -0,0 +1,14 @@
+class Basic_datatypes{
+    
+    
+    public static void main(String args[]){
+        
+        
+       int a=4;//integer declaration and intialization
+       String b="abc";//String declaration and intialization
+       char c='d';//Character declaration and intialization
+       float d=4;//float(decimal) declaration and intialization
+       System.out.println("integer is "+a+"\nString is "+b+"\ncharacter is "+c+"\nfloat is "+d);
+       
+    }
+}
